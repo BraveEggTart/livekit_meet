8591b94029afff5c7a3c8fac85db554f23a3b6c3
Emo <zp5fyb@163.com>
2025-01-21 15:30:34 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ import styles from '../styles/Home.module.css';
 export default function Page() {
   const router = useRouter();
   useEffect(() => {
-    const serverUrl = "wss://ai.just4dream.club";
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3Njg5NTY2MTUsImlzcyI6IkFQSUNtTXIyNFd6Sk1vcyIsIm5hbWUiOiJUZXN0IFVzZXIiLCJuYmYiOjE3MzI5NTY2MTUsInN1YiI6InRlc3QtdXNlciIsInZpZGVvIjp7InJvb20iOiJteS1maXJzdC1yb29tIiwicm9vbUpvaW4iOnRydWV9fQ.VMzA5ZJ_oeq40FAgve0YPmeya2lceMFi2JLqHz32stA";
+    const serverUrl = "wss://livekit.just4dream.club";
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NzM0NDExNzIsImlzcyI6IkFQSVhLVzY5aTVMNzdicSIsIm5hbWUiOiJUZXN0IFVzZXIiLCJuYmYiOjE3Mzc0NDExNzIsInN1YiI6InRlc3QtdXNlciIsInZpZGVvIjp7InJvb20iOiJteS1maXJzdC1yb29tIiwicm9vbUpvaW4iOnRydWV9fQ.siHRZwb4UB-KaStbmFfPR4elQ0ji2ne1museWTcdrwo";
     router.push(
       `/custom/?liveKitUrl=${serverUrl}&token=${token}#${encodePassphrase("7&UIJHd78i7y")}`,
     );
